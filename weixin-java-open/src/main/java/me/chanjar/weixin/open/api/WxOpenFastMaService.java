2288f123c63849e36a43e1f56eb6fc0258088272
Hipple <1159828430@qq.com>
2019-01-24 21:35:56 +0800

    Add 实现小程序名称设置及改名、微信认证名称检测、修改头像、修改功能介绍接口

--- weixin-java-open/src/main/java/me/chanjar/weixin/open/api/WxOpenFastMaService.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/api/WxOpenFastMaService.java
@@ -17,71 +17,108 @@ import me.chanjar.weixin.open.bean.fastma.WxAccountBasicInfo;
 public interface WxOpenFastMaService extends WxMaService {
 
   /**
-   * 17 获取帐号基本信息
-   * <pre>
-   *
-   * </pre>
+   * 1 获取帐号基本信息
    */
   String OPEN_GET_ACCOUNT_BASIC_INFO = "https://api.weixin.qq.com/cgi-bin/account/getaccountbasicinfo";
 
   /**
-   * 18 小程序名称设置及改名
+   * 2 小程序名称设置及改名
    */
   String OPEN_SET_NICKNAME = "https://api.weixin.qq.com/wxa/setnickname";
 
   /**
-   * 19 小程序改名审核状态查询
+   * 3 小程序改名审核状态查询
    */
   String OPEN_API_WXA_QUERYNICKNAME = "https://api.weixin.qq.com/wxa/api_wxa_querynickname";
 
   /**
-   * 20 微信认证名称检测
+   * 4 微信认证名称检测
    */
   String OPEN_CHECK_WX_VERIFY_NICKNAME = "https://api.weixin.qq.com/cgi-bin/wxverify/checkwxverifynickname";
 
   /**
-   * 21 修改头像
+   * 5 修改头像
    */
   String OPEN_MODIFY_HEADIMAGE = "https://api.weixin.qq.com/cgi-bin/account/modifyheadimage";
 
   /**
-   * 22 修改功能介绍
+   * 6修改功能介绍
    */
   String OPEN_MODIFY_SIGNATURE = "https://api.weixin.qq.com/cgi-bin/account/modifysignature";
 
   /**
-   * 23 换绑小程序管理员接口
+   * 7 换绑小程序管理员接口
    * <pre>
    *     TODO 暂不实现
    * </pre>
    */
   /**
-   * 24.1 获取账号可以设置的所有类目
+   * 8.1 获取账号可以设置的所有类目
    */
   String OPEN_GET_ALL_CATEGORIES = "https://api.weixin.qq.com/cgi-bin/wxopen/getallcategories";
   /**
-   * 24.2 添加类目
+   * 8.2 添加类目
    */
   String OPEN_ADD_CATEGORY = "https://api.weixin.qq.com/cgi-bin/wxopen/addcategory";
   /**
-   * 24.3 删除类目
+   * 8.3 删除类目
    */
   String OPEN_DELETE_CATEGORY = "https://api.weixin.qq.com/cgi-bin/wxopen/deletecategory";
   /**
-   * 24.4 获取账号已经设置的所有类目
+   * 8.4 获取账号已经设置的所有类目
    */
   String OPEN_GET_CATEGORY = "https://api.weixin.qq.com/cgi-bin/wxopen/getcategory";
   /**
-   * 24.5 修改类目
+   * 8.5 修改类目
    */
   String OPEN_MODIFY_CATEGORY = "https://api.weixin.qq.com/cgi-bin/wxopen/modifycategory";
 
 
   /**
-   * 获取小程序的信息
+   * 1.获取小程序的信息
    *
    * @return
    * @throws WxErrorException
    */
   WxAccountBasicInfo getAccountBasicInfo() throws WxErrorException;
+
+  /**
+   * 2.小程序名称设置及改名
+   * @param nickname 昵称
+   * @param idCard 身份证照片–临时素材mediaid(个人号必填)
+   * @param license 组织机构代码证或营业执照–临时素材mediaid(组织号必填)
+   * @param namingOtherStuff1 其他证明材料---临时素材 mediaid
+   * @param namingOtherStuff2 其他证明材料---临时素材 mediaid
+   * @throws WxErrorException
+   */
+  void setNickname(String nickname, String idCard, String license, String namingOtherStuff1, String namingOtherStuff2) throws WxErrorException;
+
+  /**
+   * 4. 微信认证名称检测
+   * @param nickname 名称
+   * @throws WxErrorException
+   */
+  String checkWxVerifyNickname(String nickname) throws WxErrorException;
+
+  /**
+   * 5.修改头像
+   * <pre>
+   *     图片格式只支持：BMP、JPEG、JPG、GIF、PNG，大小不超过2M
+   *      注：实际头像始终为正方形
+   * </pre>
+   * @param headImgMediaId 头像素材media_id
+   * @param x1 裁剪框左上角x坐标（取值范围：[0, 1]）
+   * @param y1 裁剪框左上角y坐标（取值范围：[0, 1]）
+   * @param x2 裁剪框右下角x坐标（取值范围：[0, 1]）
+   * @param y2 裁剪框右下角y坐标（取值范围：[0, 1]）
+   * @throws WxErrorException
+   */
+  void modifyHeadImage(String headImgMediaId, float x1, float y1, float x2, float y2) throws WxErrorException;
+
+  /**
+   * 6.修改功能介绍
+   * @param signature 简介：4-120字
+   * @throws WxErrorException
+   */
+  void modifySignature(String signature) throws WxErrorException;
 }
--- weixin-java-open/src/main/java/me/chanjar/weixin/open/api/impl/WxOpenFastMaServiceImpl.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/api/impl/WxOpenFastMaServiceImpl.java
@@ -3,7 +3,10 @@ package me.chanjar.weixin.open.api.impl;
 import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
 import cn.binarywang.wx.miniapp.config.WxMaConfig;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.common.util.json.GsonHelper;
 import me.chanjar.weixin.open.api.WxOpenComponentService;
 import me.chanjar.weixin.open.api.WxOpenFastMaService;
 import me.chanjar.weixin.open.bean.fastma.WxAccountBasicInfo;
@@ -22,37 +25,112 @@ public class WxOpenFastMaServiceImpl extends WxMaServiceImpl implements WxOpenFa
   private WxMaConfig wxMaConfig;
   private String appId;
 
-  public WxOpenFastMaServiceImpl(WxOpenComponentService wxOpenComponentService, String appId, WxMaConfig wxMaConfig) {
+  public WxOpenFastMaServiceImpl (WxOpenComponentService wxOpenComponentService, String appId, WxMaConfig wxMaConfig) {
     this.wxOpenComponentService = wxOpenComponentService;
     this.appId = appId;
     this.wxMaConfig = wxMaConfig;
-    initHttp();
+    initHttp ();
   }
 
   @Override
-  public WxMaConfig getWxMaConfig() {
+  public WxMaConfig getWxMaConfig () {
     return wxMaConfig;
   }
 
   @Override
-  public String getAccessToken(boolean forceRefresh) throws WxErrorException {
-    return wxOpenComponentService.getAuthorizerAccessToken(appId, forceRefresh);
+  public String getAccessToken (boolean forceRefresh) throws WxErrorException {
+    return wxOpenComponentService.getAuthorizerAccessToken (appId, forceRefresh);
   }
 
   /**
-   * 获取小程序的信息,GET请求
+   * 1.获取小程序的信息,GET请求
    * <pre>
    *     注意：这里不能直接用小程序的access_token
-   *
    * </pre>
    *
    * @return
    * @throws WxErrorException
    */
   @Override
-  public WxAccountBasicInfo getAccountBasicInfo() throws WxErrorException {
-    String response = get(OPEN_GET_ACCOUNT_BASIC_INFO, "");
-    return WxOpenGsonBuilder.create().fromJson(response, WxAccountBasicInfo.class);
+  public WxAccountBasicInfo getAccountBasicInfo () throws WxErrorException {
+    String response = get (OPEN_GET_ACCOUNT_BASIC_INFO, "");
+    return WxOpenGsonBuilder.create ().fromJson (response, WxAccountBasicInfo.class);
+  }
+
+  /**
+   * 2.小程序名称设置及改名
+   *  TODO
+   * @param nickname          昵称
+   * @param idCard            身份证照片–临时素材mediaid(个人号必填)
+   * @param license           组织机构代码证或营业执照–临时素材mediaid(组织号必填)
+   * @param namingOtherStuff1 其他证明材料---临时素材 mediaid
+   * @param namingOtherStuff2 其他证明材料---临时素材 mediaid
+   * @throws WxErrorException
+   */
+  @Override
+  public void setNickname (String nickname, String idCard, String license, String namingOtherStuff1, String namingOtherStuff2) throws WxErrorException {
+    JsonObject params = new JsonObject ();
+    params.addProperty ("nick_name", nickname);
+    params.addProperty ("id_card", idCard);
+    params.addProperty ("license", license);
+    params.addProperty ("naming_other_stuff_1", namingOtherStuff1);
+    params.addProperty ("naming_other_stuff_2", namingOtherStuff2);
+    String response = post (OPEN_SET_NICKNAME, GSON.toJson (params));
+  }
+
+  /**
+   * 4. 微信认证名称检测
+   *  <pre>
+   *      命中关键字策略时返回命中关键字的说明描述
+   *  </pre>
+   * @param nickname 名称
+   * @throws WxErrorException
+   */
+  @Override
+  public String checkWxVerifyNickname (String nickname) throws WxErrorException {
+    JsonObject params = new JsonObject ();
+    params.addProperty ("nick_name", nickname);
+    String response = post (OPEN_CHECK_WX_VERIFY_NICKNAME, GSON.toJson (params));
+    JsonObject resObj = new JsonParser ().parse(response).getAsJsonObject();
+    return GsonHelper.getString (resObj, "wording");
+  }
+
+  /**
+   * 5.修改头像
+   * <pre>
+   *     图片格式只支持：BMP、JPEG、JPG、GIF、PNG，大小不超过2M
+   *      注：实际头像始终为正方形
+   * </pre>
+   *
+   * @param headImgMediaId 头像素材media_id
+   * @param x1             裁剪框左上角x坐标（取值范围：[0, 1]）
+   * @param y1             裁剪框左上角y坐标（取值范围：[0, 1]）
+   * @param x2             裁剪框右下角x坐标（取值范围：[0, 1]）
+   * @param y2             裁剪框右下角y坐标（取值范围：[0, 1]）
+   * @throws WxErrorException
+   */
+  @Override
+  public void modifyHeadImage (String headImgMediaId, float x1, float y1, float x2, float y2) throws WxErrorException {
+    JsonObject params = new JsonObject ();
+    params.addProperty ("head_img_media_id", headImgMediaId);
+    params.addProperty ("x1", x1);
+    params.addProperty ("y1", y1);
+    params.addProperty ("x2", x2);
+    params.addProperty ("y2", y2);
+    post (OPEN_MODIFY_HEADIMAGE, GSON.toJson (params));
+  }
+
+  /**
+   * 6.修改功能介绍
+   *
+   * @param signature 简介：4-120字
+   * @throws WxErrorException
+   */
+  @Override
+  public void modifySignature (String signature) throws WxErrorException {
+    JsonObject params = new JsonObject ();
+    params.addProperty ("signature", signature);
+    post (OPEN_MODIFY_SIGNATURE, GSON.toJson (params));
   }
 
   /**
@@ -61,11 +139,11 @@ public class WxOpenFastMaServiceImpl extends WxMaServiceImpl implements WxOpenFa
    * @param strList
    * @return
    */
-  private JsonArray toJsonArray(List<String> strList) {
-    JsonArray jsonArray = new JsonArray();
-    if (strList != null && !strList.isEmpty()) {
+  private JsonArray toJsonArray (List<String> strList) {
+    JsonArray jsonArray = new JsonArray ();
+    if (strList != null && ! strList.isEmpty ()) {
       for (String str : strList) {
-        jsonArray.add(str);
+        jsonArray.add (str);
       }
     }
     return jsonArray;
