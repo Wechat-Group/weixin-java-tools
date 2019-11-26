86a8b07c535d24f8a75d0d5c9d6ccb99088c7149
xiaoyu <304491159@qq.com>
2019-11-26 18:43:04 +0800

    增加微信文档地址说明

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/api/WxMaExpressService.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/api/WxMaExpressService.java
@@ -40,6 +40,9 @@ public interface WxMaExpressService {
    * 获取支持的快递公司列表
    * @return  快递公司列表
    * @throws WxErrorException 获取失败时返回
+   * <pre>
+   *     文档地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/express/by-business/logistics.getAllDelivery.html
+   * </pre>
    */
   List<WxMaExpressDelivery> getAllDelivery() throws WxErrorException;
 
@@ -47,6 +50,9 @@ public interface WxMaExpressService {
    * 获取所有绑定的物流账号
    * @return 物流账号list
    * @throws WxErrorException 获取失败时返回
+   * <pre>
+   *     文档地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/express/by-business/logistics.getAllAccount.html
+   * </pre>
    */
   List<WxMaExpressAccount> getAllAccount() throws WxErrorException;
 
@@ -54,6 +60,9 @@ public interface WxMaExpressService {
    * 绑定、解绑物流账号
    * @param wxMaExpressBindAccountRequest 物流账号对象
    * @throws WxErrorException 请求失败时返回
+   * <pre>
+   *     文档地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/express/by-business/logistics.bindAccount.html
+   * </pre>
    */
   void bindAccount(WxMaExpressBindAccountRequest wxMaExpressBindAccountRequest) throws WxErrorException;
 
@@ -61,12 +70,19 @@ public interface WxMaExpressService {
    * 配置面单打印员，可以设置多个，若需要使用微信打单 PC 软件，才需要调用。
    * @param wxMaExpressPrinterUpdateRequest  面单打印员对象
    * @throws WxErrorException 请求失败时返回
+   * <pre>
+   *     文档地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/express/by-business/logistics.updatePrinter.html
+   * </pre>
    */
   void updatePrinter(WxMaExpressPrinterUpdateRequest wxMaExpressPrinterUpdateRequest) throws WxErrorException;
 
   /**
    * 获取打印员。若需要使用微信打单 PC 软件，才需要调用
+   * @return 打印员
    * @throws WxErrorException 获取失败时返回
+   * <pre>
+   *     文档地址：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/express/by-business/logistics.getPrinter.html
+   * </pre>
    */
   WxMaExpressPrinter getPrinter() throws WxErrorException;
 }
