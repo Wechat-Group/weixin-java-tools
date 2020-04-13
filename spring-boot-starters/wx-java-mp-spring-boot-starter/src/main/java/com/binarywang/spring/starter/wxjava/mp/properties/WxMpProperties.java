2142bc942def2ca318693fad4ef9b65e18272be5
Mario Luo <luokaiqiongmou@foxmail.com>
2020-04-13 14:06:19 +0800

    redis支持配置key前缀、RedisTemplate集成

--- spring-boot-starters/wx-java-mp-spring-boot-starter/src/main/java/com/binarywang/spring/starter/wxjava/mp/properties/WxMpProperties.java
+++ spring-boot-starters/wx-java-mp-spring-boot-starter/src/main/java/com/binarywang/spring/starter/wxjava/mp/properties/WxMpProperties.java
@@ -2,7 +2,6 @@ package com.binarywang.spring.starter.wxjava.mp.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.io.Serializable;
 
@@ -41,29 +40,84 @@ public class WxMpProperties {
   private String aesKey;
 
   /**
-   * 存储策略, memory, redis.
+   * 存储策略
    */
-  private final ConfigStorage configStorage = new ConfigStorage();
-
+  private ConfigStorage configStorage = new ConfigStorage();
 
   @Data
   public static class ConfigStorage implements Serializable {
     private static final long serialVersionUID = 4815731027000065434L;
 
+    /**
+     * 存储类型
+     */
     private StorageType type = memory;
 
-    @NestedConfigurationProperty
-    private final RedisProperties redis = new RedisProperties();
+    /**
+     * 指定key前缀
+     */
+    private String keyPrefix = "wx";
+
+    /**
+     * redis连接配置
+     */
+    private RedisProperties redis = new RedisProperties();
   }
 
   public enum StorageType {
     /**
-     * 内存.
+     * 内存
      */
     memory,
     /**
-     * redis.
+     * jedis
      */
-    redis
+    @Deprecated
+    redis,
+    /**
+     * redis(Jedis Client)
+     */
+    jedis,
+    /**
+     * redis(RedisTemplate)
+     */
+    redis_template,
+    ;
+  }
+
+
+  @Data
+  public static class RedisProperties implements Serializable {
+    private static final long serialVersionUID = -5924815351660074401L;
+
+    /**
+     * 主机地址.
+     */
+    private String host = "127.0.0.1";
+
+    /**
+     * 端口号.
+     */
+    private int port = 6379;
+
+    /**
+     * 密码.
+     */
+    private String password;
+
+    /**
+     * 超时.
+     */
+    private int timeout = 2000;
+
+    /**
+     * 数据库.
+     */
+    private int database = 0;
+
+    private Integer maxActive;
+    private Integer maxIdle;
+    private Integer maxWaitMillis;
+    private Integer minIdle;
   }
 }
