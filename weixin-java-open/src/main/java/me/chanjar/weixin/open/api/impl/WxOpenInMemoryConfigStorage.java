5d64cc8c59637ae7f02a03205fd3ca25c95e59ff
Element <my-tangjianbin@163.com>
2020-07-21 00:43:06 +0800

    补充 me.chanjar.weixin.open.api.impl.WxOpenInMemoryConfigStorage#updateToken 方法对 expireInSeconds 边界判断

--- weixin-java-open/src/main/java/me/chanjar/weixin/open/api/impl/WxOpenInMemoryConfigStorage.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/api/impl/WxOpenInMemoryConfigStorage.java
@@ -142,7 +142,7 @@ public class WxOpenInMemoryConfigStorage implements WxOpenConfigStorage {
       map.put(key, token);
     }
     token.token = tokenString;
-    if (expiresInSeconds != null) {
+    if (expiresInSeconds != null && expiresInSeconds != -1) {
       token.expiresTime = System.currentTimeMillis() + (expiresInSeconds - 200) * 1000L;
     }
   }
@@ -159,7 +159,7 @@ public class WxOpenInMemoryConfigStorage implements WxOpenConfigStorage {
 
   @Override
   public void updateAuthorizerRefreshToken(String appId, String authorizerRefreshToken) {
-    this.setAuthorizerRefreshToken(appId,authorizerRefreshToken);
+    this.setAuthorizerRefreshToken(appId, authorizerRefreshToken);
   }
 
   @Override
