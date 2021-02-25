e90f0507df1c0bf7be430abd17789d9f1a00d6fd
张强 <zhangq@yxt.com>
2021-02-25 11:10:17 +0800

    :art: 【企业微信】完善标签管理相关的单元测试

---
+++ weixin-java-cp/src/test/java/me/chanjar/weixin/cp/tp/service/impl/WxCpTpTagServiceImplTest.java
@@ -0,0 +1,41 @@
+package me.chanjar.weixin.cp.tp.service.impl;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * 企业微信-第三方开发-标签管理相关测试
+ * @author zhangq
+ * @since 2021/2/15 9:14
+ */
+public class WxCpTpTagServiceImplTest {
+
+	@Test
+	public void testCreate() {
+	}
+
+	@Test
+	public void testUpdate() {
+	}
+
+	@Test
+	public void testDelete() {
+	}
+
+	@Test
+	public void testListAll() {
+	}
+
+	@Test
+	public void testGet() {
+	}
+
+	@Test
+	public void testAddUsers2Tag() {
+	}
+
+	@Test
+	public void testRemoveUsersFromTag() {
+	}
+}
