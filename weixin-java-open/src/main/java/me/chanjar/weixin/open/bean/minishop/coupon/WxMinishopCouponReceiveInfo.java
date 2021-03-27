3642ef6a056c0854532ad3147b8bdd96dc9604cd
luowentao <luowentao@gzwangniu.com>
2021-03-27 18:55:47 +0800

    修复空指针的bug

--- weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/minishop/coupon/WxMinishopCouponReceiveInfo.java
+++ weixin-java-open/src/main/java/me/chanjar/weixin/open/bean/minishop/coupon/WxMinishopCouponReceiveInfo.java
@@ -11,13 +11,13 @@ import java.io.Serializable;
 @ApiModel("小商店优惠券领取信息")
 public class WxMinishopCouponReceiveInfo implements Serializable {
   @ApiModelProperty(value = "优惠券领用结束时间", required = true)
-  private String endTime;
+  private Long endTime;
 
   @ApiModelProperty(value = "是否限制一人使用", required = true)
   private Integer limitNumOnePerson;
 
   @ApiModelProperty(value = "优惠券领用开始时间",required = true)
-  private String startTime;
+  private Long startTime;
 
   @ApiModelProperty(value = "优惠券领用总数", required = true)
   private Integer totalNum;
