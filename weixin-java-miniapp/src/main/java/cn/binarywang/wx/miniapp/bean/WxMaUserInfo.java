789b822941ea64cdb8cc172ee016ff1cb4d0110f
刘小狼狈 <258392906@qq.com>
2021-04-19 15:58:02 +0800

    新版微信小程序API已经不再返回 openId,unionId
    https://developers.weixin.qq.com/miniprogram/dev/api/open-api/user-info/UserInfo.html

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaUserInfo.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaUserInfo.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 public class WxMaUserInfo implements Serializable {
   private static final long serialVersionUID = 6719822331555402137L;
 
-  private String openId;
+//  private String openId;
   private String nickName;
   private String gender;
   private String language;
@@ -20,7 +20,7 @@ public class WxMaUserInfo implements Serializable {
   private String province;
   private String country;
   private String avatarUrl;
-  private String unionId;
+//  private String unionId;
   private Watermark watermark;
 
   public static WxMaUserInfo fromJson(String json) {
