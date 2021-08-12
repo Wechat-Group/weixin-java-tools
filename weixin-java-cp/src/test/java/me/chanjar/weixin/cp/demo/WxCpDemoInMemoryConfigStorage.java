5ecd69a67828199ae7d9497b78cf032059193ae4
caiqiyuan <cqyisbug@163.com>
2021-08-12 16:35:09 +0800

    optimize

--- weixin-java-cp/src/test/java/me/chanjar/weixin/cp/demo/WxCpDemoInMemoryConfigStorage.java
+++ weixin-java-cp/src/test/java/me/chanjar/weixin/cp/demo/WxCpDemoInMemoryConfigStorage.java
@@ -1,9 +1,8 @@
 package me.chanjar.weixin.cp.demo;
 
-import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import lombok.ToString;
-import me.chanjar.weixin.common.util.xml.XStreamInitializer;
+import me.chanjar.weixin.common.util.xml.XmlUtil;
 import me.chanjar.weixin.cp.config.impl.WxCpDefaultConfigImpl;
 
 import java.io.InputStream;
@@ -15,9 +14,7 @@ import java.io.InputStream;
 @ToString
 public class WxCpDemoInMemoryConfigStorage extends WxCpDefaultConfigImpl {
   public static WxCpDemoInMemoryConfigStorage fromXml(InputStream is) {
-    XStream xstream = XStreamInitializer.getInstance();
-    xstream.processAnnotations(WxCpDemoInMemoryConfigStorage.class);
-    return (WxCpDemoInMemoryConfigStorage) xstream.fromXML(is);
+    return XmlUtil.toObject(is, WxCpDemoInMemoryConfigStorage.class);
   }
 
 }
