92076fa3fba8ee23b01aedce601cbcf415ec17cd
caiqiyuan <cqyisbug@163.com>
2021-08-15 12:25:23 +0800

    fix compile bug

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/util/xml/XmlUtil.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/util/xml/XmlUtil.java
@@ -1,6 +1,9 @@
 package me.chanjar.weixin.common.util.xml;
 
+import com.thoughtworks.xstream.XStream;
+
 import java.io.InputStream;
+import java.util.function.Consumer;
 
 /**
  * @author caiqy
@@ -8,17 +11,28 @@ import java.io.InputStream;
 public class XmlUtil {
 
   public static String toXml(Object obj) {
-    return XStreamInitializer.getInstance(obj.getClass()).toXML(obj);
+    return toXml(obj, null);
   }
 
-  @SuppressWarnings("unchecked")
+  public static String toXml(Object obj, Consumer<XStream> consumer) {
+    return XStreamInitializer.getInstance(obj.getClass(), consumer).toXML(obj);
+  }
+
   public static <T> T toObject(String xml, Class<T> clz) {
-    return (T) XStreamInitializer.getInstance(clz).fromXML(xml);
+    return toObject(xml, clz, null);
   }
 
   @SuppressWarnings("unchecked")
-  public static <T> T toObject(InputStream is, Class<T> clz) {
-    return (T) XStreamInitializer.getInstance(clz).fromXML(is);
+  public static <T> T toObject(String xml, Class<T> clz, Consumer<XStream> consumer) {
+    return (T) XStreamInitializer.getInstance(clz, consumer).fromXML(xml);
   }
 
+  public static <T> T toObject(InputStream is, Class<T> clz) {
+    return toObject(is, clz, null);
+  }
+
+  @SuppressWarnings("unchecked")
+  public static <T> T toObject(InputStream is, Class<T> clz, Consumer<XStream> consumer) {
+    return (T) XStreamInitializer.getInstance(clz, consumer).fromXML(is);
+  }
 }
