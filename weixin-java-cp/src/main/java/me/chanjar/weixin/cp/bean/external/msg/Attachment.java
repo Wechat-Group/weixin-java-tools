230d4ceecf87f2316c828a65fde6756dc22c43bf
caiqiyuan <cqyisbug@163.com>
2021-08-19 10:48:17 +0800

    调整企业群发接口

--- weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/external/msg/Attachment.java
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/external/msg/Attachment.java
@@ -1,50 +1,60 @@
 package me.chanjar.weixin.cp.bean.external.msg;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
 import me.chanjar.weixin.cp.constant.WxCpConsts;
 
 import java.io.Serializable;
 
+@Getter
 public class Attachment implements Serializable {
   private static final long serialVersionUID = -8078748379570640198L;
 
   @SerializedName("msgtype")
-  private String msgType;
+  private final String msgType;
 
   @SerializedName("image")
-  private Image image;
+  private final Image image;
 
   @SerializedName("link")
-  private Link link;
+  private final Link link;
 
   @SerializedName("miniprogram")
-  private MiniProgram miniprogram;
+  private final MiniProgram miniprogram;
 
   @SerializedName("video")
-  private Video video;
+  private final Video video;
 
-  private Attachment(String msgType, Image image, Link link, MiniProgram miniprogram, Video video) {
+  @SerializedName("file")
+  private final File file;
+
+  private Attachment(String msgType, Image image, Link link, MiniProgram miniprogram, Video video, File file) {
     this.msgType = msgType;
     this.image = image;
     this.link = link;
     this.miniprogram = miniprogram;
     this.video = video;
+    this.file = file;
   }
 
+
   public static Attachment image(Image image) {
-    return new Attachment(WxCpConsts.AttachmentMsgType.IMAGE.getName(), image, null, null, null);
+    return new Attachment(WxCpConsts.AttachmentMsgType.IMAGE.getName(), image, null, null, null, null);
   }
 
   public static Attachment link(Link link) {
-    return new Attachment(WxCpConsts.AttachmentMsgType.LINK.getName(), null, link, null, null);
+    return new Attachment(WxCpConsts.AttachmentMsgType.LINK.getName(), null, link, null, null, null);
   }
 
   public static Attachment miniprogram(MiniProgram miniProgram) {
-    return new Attachment(WxCpConsts.AttachmentMsgType.MINIPROGRAM.getName(), null, null, miniProgram, null);
+    return new Attachment(WxCpConsts.AttachmentMsgType.MINIPROGRAM.getName(), null, null, miniProgram, null, null);
   }
 
   public static Attachment video(Video video) {
-    return new Attachment(WxCpConsts.AttachmentMsgType.VIDEO.getName(), null, null, null, video);
+    return new Attachment(WxCpConsts.AttachmentMsgType.VIDEO.getName(), null, null, null, video, null);
   }
 
+  public static Attachment file(File file) {
+    return new Attachment(WxCpConsts.AttachmentMsgType.FILE.getName(), null, null, null, null, file);
+  }
 }
