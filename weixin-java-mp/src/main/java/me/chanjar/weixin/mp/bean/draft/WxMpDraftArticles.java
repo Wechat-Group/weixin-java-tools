db340e2fe57836654d1db1ea236876470a6c31e7
Binary Wang <binarywang@users.noreply.github.com>
2021-12-10 16:08:43 +0800

    Revert ":art: Update WxMpDraftArticles to add url"
    This reverts commit 170f241027f9765dd0d0033c5284e3da26e77bbb.

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/draft/WxMpDraftArticles.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/draft/WxMpDraftArticles.java
@@ -68,8 +68,6 @@ public class WxMpDraftArticles implements ToJson, Serializable {
    */
   @SerializedName("only_fans_can_comment")
   private Integer onlyFansCanComment;
-  
-  private String url;
 
   public static WxMpDraftArticles fromJson(String json) {
     return WxGsonBuilder.create().fromJson(json, WxMpDraftArticles.class);
