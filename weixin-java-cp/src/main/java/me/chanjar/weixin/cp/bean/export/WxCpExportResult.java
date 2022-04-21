b20905a082b78749b5beff1e782e5d1aac3b497a
zhongjun96 <zhongjun96@aliyun.com>
2022-04-21 19:05:36 +0800

    :art: 添加注释

--- weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/export/WxCpExportResult.java
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/export/WxCpExportResult.java
@@ -18,7 +18,9 @@ import java.util.List;
 public class WxCpExportResult extends WxCpBaseResp {
   private static final long serialVersionUID = -8673839248829238966L;
 
-
+  /**
+   * 任务状态:0-未处理，1-处理中，2-完成，3-异常失败
+   */
   private Integer status;
 
   @SerializedName("data_list")
@@ -28,10 +30,19 @@ public class WxCpExportResult extends WxCpBaseResp {
   @Data
   public static class ExportData {
 
+    /**
+     * 数据下载链接,支持指定Range头部分段下载。有效期2个小时
+     */
     private String url;
 
+    /**
+     * 密文数据大小
+     */
     private Integer size;
 
+    /**
+     * 密文数据md5
+     */
     private String md5;
   }
 }
