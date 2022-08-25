687efdee1cb47e9aaa3bf068d5a621ed0f5c664f
liming <liming1019@126.com>
2022-08-25 15:00:52 +0800

    【小程序】增加新版自定义交易组件获取售后单详情API https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/business-capabilities/ministore/minishopopencomponent2/API/aftersale/get_new.html

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/api/WxMaShopAfterSaleService.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/api/WxMaShopAfterSaleService.java
@@ -1,16 +1,7 @@
 package cn.binarywang.wx.miniapp.api;
 
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopAcceptReturnRequest;
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopAfterSaleAddRequest;
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopAfterSaleGetRequest;
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopAfterSaleListRequest;
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopAfterSaleUpdateRequest;
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopAfterSaleUploadReturnInfoRequest;
-import cn.binarywang.wx.miniapp.bean.shop.request.WxMaShopUploadCerficatesRequest;
-import cn.binarywang.wx.miniapp.bean.shop.response.WxMaShopAfterSaleAddResponse;
-import cn.binarywang.wx.miniapp.bean.shop.response.WxMaShopAfterSaleGetResponse;
-import cn.binarywang.wx.miniapp.bean.shop.response.WxMaShopAfterSaleListResponse;
-import cn.binarywang.wx.miniapp.bean.shop.response.WxMaShopBaseResponse;
+import cn.binarywang.wx.miniapp.bean.shop.request.*;
+import cn.binarywang.wx.miniapp.bean.shop.response.*;
 import me.chanjar.weixin.common.error.WxErrorException;
 
 /**
@@ -39,6 +30,15 @@ public interface WxMaShopAfterSaleService {
   WxMaShopAfterSaleGetResponse get(WxMaShopAfterSaleGetRequest request) throws WxErrorException;
 
   /**
+   * 获取售后单详情(EC版)
+   *
+   * @param request
+   * @return WxMaShopEcAfterSaleGetResponse
+   * @throws WxErrorException
+   */
+  WxMaShopEcAfterSaleGetResponse get(WxMaShopEcAfterSaleGetRequest request) throws WxErrorException;
+
+  /**
    * 更新售后
    *
    * @param request
@@ -49,9 +49,10 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 用户取消售后申请
+   *
    * @param outAfterSaleId 商家自定义订单ID
-   * @param afterSaleId 与out_aftersale_id二选一
-   * @param openId 用户openid
+   * @param afterSaleId    与out_aftersale_id二选一
+   * @param openId         用户openid
    * @return
    * @throws WxErrorException
    */
@@ -60,6 +61,7 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 用户上传退货物流
+   *
    * @param request
    * @return
    * @throws WxErrorException
@@ -69,6 +71,7 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 商家同意退款
+   *
    * @param outAfterSaleId
    * @param afterSaleId
    * @return
@@ -79,6 +82,7 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 商家同意退货
+   *
    * @param request
    * @return
    * @throws WxErrorException
@@ -88,6 +92,7 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 商家拒绝售后
+   *
    * @param outAfterSaleId
    * @param afterSaleId
    * @return
@@ -98,6 +103,7 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 商家上传退款凭证
+   *
    * @param request
    * @return
    * @throws WxErrorException
@@ -107,6 +113,7 @@ public interface WxMaShopAfterSaleService {
 
   /**
    * 商家更新订单售后期
+   *
    * @param outOrderId
    * @param orderId
    * @param openid
@@ -115,10 +122,11 @@ public interface WxMaShopAfterSaleService {
    * @throws WxErrorException
    */
   WxMaShopBaseResponse updateDeadline(String outOrderId, Long orderId, String openid,
-    Long afterSaleDeadline) throws WxErrorException;
+                                      Long afterSaleDeadline) throws WxErrorException;
 
   /**
    * 获取售后单详情
+   *
    * @param request
    * @return
    * @throws WxErrorException
