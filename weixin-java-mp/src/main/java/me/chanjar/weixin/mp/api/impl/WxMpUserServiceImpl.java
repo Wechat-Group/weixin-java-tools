e1907e6a2ff4c1f3b796da8139ac15162a7736a2
Cocowwy <514658459@qq.com>
2022-09-07 17:42:39 +0800

    【feat】me.chanjar.weixin.mp.api.WxMpUserService.userList(java.lang.String) 重载了一个方法，屏蔽多次需要多次拉取，对于调用方，可一次性拉取所有用户数据

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpUserServiceImpl.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpUserServiceImpl.java
@@ -10,6 +10,7 @@ import me.chanjar.weixin.mp.bean.result.WxMpChangeOpenid;
 import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import me.chanjar.weixin.mp.bean.result.WxMpUserList;
 import me.chanjar.weixin.mp.util.json.WxMpGsonBuilder;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -50,6 +51,25 @@ public class WxMpUserServiceImpl implements WxMpUserService {
   public WxMpUserList userList(String nextOpenid) throws WxErrorException {
     String responseContent = this.wxMpService.get(USER_GET_URL, nextOpenid == null ? null : "next_openid=" + nextOpenid);
     return WxMpUserList.fromJson(responseContent);
+  }
+
+  @Override
+  public WxMpUserList userList() throws WxErrorException {
+    String responseContent = this.wxMpService.get(USER_GET_URL, null);
+    WxMpUserList merageList = new WxMpUserList();
+
+    WxMpUserList wxMpUserList = WxMpUserList.fromJson(responseContent);
+    merageList.getOpenids().addAll(wxMpUserList.getOpenids());
+    merageList.setCount(wxMpUserList.getCount());
+    merageList.setTotal(wxMpUserList.getTotal());
+
+    while (StringUtils.isNotEmpty(wxMpUserList.getNextOpenid())) {
+      WxMpUserList nextReqUserList = userList(wxMpUserList.getNextOpenid());
+      merageList.getOpenids().addAll(nextReqUserList.getOpenids());
+      merageList.setCount(merageList.getCount() + nextReqUserList.getCount());
+      wxMpUserList = nextReqUserList;
+    }
+    return merageList;
   }
 
   @Override
